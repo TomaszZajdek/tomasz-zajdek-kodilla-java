b9f7549b0f55e925ccd8ad1c0a986beca0da6a75
Tomasz <luluclucja@icloud.com>
2023-10-11 20:41:39 +0200

    6.2 added Calculator class + testing method

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
-
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(13, 7);
+        int resultSubtract = calculator.subtract(13, 7);
+
+        if (resultAdd == 20) {
+            System.out.println("Test add OK");
+        } else {
+            System.out.println("Test add ERROR");
+        }
+
+        if (resultSubtract == 6) {
+            System.out.println("Test subtract OK");
+        } else {
+            System.out.println("Test subtract ERROR");
+        }
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CollectionTestSuite {
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end");
+    }
+    @Test
+    @DisplayName()
+
+    testOddNumbersExterminatorEmptyList
+
+            testOddNumbersExterminatorNormalList
+}
