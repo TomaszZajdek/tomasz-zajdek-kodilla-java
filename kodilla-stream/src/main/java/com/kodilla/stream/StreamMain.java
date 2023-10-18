f005285a1898a071d00c6b105ad2a50975f02720
Tomasz <luluclucja@icloud.com>
2023-10-18 08:49:16 +0200

    7.2 added immutable class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
@@ -29,5 +30,8 @@ public class StreamMain {
         poemBeautifier.beautify(word, string -> "ABC" + string + "ABC");
         poemBeautifier.beautify(word, string -> string + "ma " + string.length() + " liter");
         poemBeautifier.beautify(word, string -> string + string + string + string);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
