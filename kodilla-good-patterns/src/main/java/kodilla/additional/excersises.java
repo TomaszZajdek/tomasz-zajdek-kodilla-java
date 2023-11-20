a65818430877bfa5e7017440ccf37aea2690450e
Tomasz <luluclucja@icloud.com>
2023-11-20 15:24:33 +0100

    undid changes after reset

---
+++ kodilla-good-patterns/src/main/java/kodilla/additional/excersises.java
@@ -0,0 +1,11 @@
+package kodilla.additional;
+
+public class excersises {
+    public void factorial(int n) {
+        int result = 1;
+        for(int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println("result : " + n );
+    }
+}
